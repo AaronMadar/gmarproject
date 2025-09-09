2354e92a8a1b1715f154564320f4867ebe7d2172
Aaron <aaronmadar2655@gmail.com>
2025-09-09 11:39:04 +0300

    first-layout-nivout

--- src/application-layout/Content.tsx
+++ src/application-layout/Content.tsx
@@ -1,4 +1,4 @@
-export default function Content({children}){
+export default function Content({children}:any){
 
 
     return (
---
+++ src/components/HomePage.tsx
@@ -0,0 +1,9 @@
+export default function HomePage(){
+
+
+    return (
+        <>
+        <h1>HELLO HOME PAGE</h1>
+        </>
+    )
+}
