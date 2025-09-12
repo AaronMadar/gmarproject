d65464b21ff9440dc2726503cec946710fe24030
Aaron <aaronmadar2655@gmail.com>
2025-09-12 17:02:55 +0300

    logout to homepage

--- src/Context/AuthContext.tsx
+++ src/Context/AuthContext.tsx
@@ -14,7 +14,7 @@ const [isAuthenticated, setIsAuthenticated]= useState(false)
 
 const login = ()=> setIsAuthenticated(true);
 
-const logout = ()=> setIsAuthenticated(false);
+const logout = ()=> { localStorage.removeItem("token") ; setIsAuthenticated(false);}
 
 
 
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,37 +1,55 @@
 import { useNavigate } from 'react-router-dom'
 import '../style/Login.css'
 import Title from './Title'
-import { useContext, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { AuthContext } from '../Context/AuthContext'
-import Actuality from './Actuality'
+
 
 
 
 
 export default function Login() {
 
-    const { login } = useContext(AuthContext)
+    const { login, isAuthenticated } = useContext(AuthContext)
     const navigate = useNavigate()
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
+    const [error, setError] = useState("")
+
+     useEffect(() => {
+        if (isAuthenticated) {
+            navigate("/feed")
+        }
+    }, [isAuthenticated, navigate])
+
 
     async function HandleClick(e) {
         e.preventDefault()
         const userdetails = { username, password };
-        const response = await fetch("http://localhost:3001/api/user/login", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(userdetails),
-        });
-        let data = await response.json()
-        if (!data.success) {
-            return <div className='error'>`${response.success} , ${response.response}`</div>
-        }
-        localStorage.setItem("token", data.token)
-        login()
-        navigate("/feed")
 
-    }           
+        try {
+
+            const response = await fetch("http://localhost:3001/api/user/login", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(userdetails),
+            });
+            let data = await response.json()
+            if (!data.success) {
+                setError('Login Failed')
+            }
+            localStorage.setItem("token", data.token)
+            login()
+            navigate("/feed")
+
+        }
+       catch{
+        console.log('Error network');
+        setError('Error network')
+        
+       }
+
+    }
 
 
     return (
@@ -42,6 +60,7 @@ export default function Login() {
                 <form onSubmit={HandleClick} className="formlogin">
                     <input type="text" placeholder='Enter your username' onChange={(e) => setUsername(e.target.value)} value={username} />
                     <input type="password" placeholder='Enter your password' onChange={(e) => setPassword(e.target.value)} value={password} />
+                    {error && <div className='error'>{error}</div>}
                     <div className='divbtn'>
                         <button type="submit" className="btnaction">Login</button>
                         <button className='btnsub' onClick={() => navigate('/Subscribe')}>--subscribe</button>
@@ -50,6 +69,6 @@ export default function Login() {
                 </form>
             </section>
 
-        </>
+        </> 
     )
 }
